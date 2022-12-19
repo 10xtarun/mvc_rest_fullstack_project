758ced3079f18aa37ce41ad534799286bbe85041
10xtarun <10xtarun@gmail.com>
2022-12-19 14:55:06 +0530

    eslint and nodemon added

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
-const express = require("express")
-const path = require("path")
-const { gracefulShutdown, shutDownHandler } = require("./utils")
-const authRouter = require("./routes/authentication")
+const express = require('express')
+const path = require('path')
+const { gracefulShutdown, shutDownHandler } = require('./utils')
+const authRouter = require('./routes/authentication')
 
-function app() {
-    let server
-    const app = express()
+function app () {
+  let server
+  const app = express()
 
-    console.log("initiating server start sequence")
-    return Promise.resolve()
+  console.log('initiating server start sequence')
+  return Promise.resolve()
     .then(() => {
-        // attach middlewares
+      // attach middlewares
     })
     .then(() => {
-        // View routes
-        app.get("/",(req, res) => {
-            return res.sendFile(path.join(__dirname, "views", "index.html"))
-        })
+      // View routes
+      app.get('/', (req, res) => {
+        return res.sendFile(path.join(__dirname, 'views', 'index.html'))
+      })
 
-        app.get("/todos", (req, res) => {
-            return res.sendFile(path.join(__dirname, "views", "todos.html"))
-        })
+      app.get('/todos', (req, res) => {
+        return res.sendFile(path.join(__dirname, 'views', 'todos.html'))
+      })
 
-        app.get("/profile", (req, res) => {
-            return res.sendFile(path.join(__dirname, "views", "profile.html"))
-        })
+      app.get('/profile', (req, res) => {
+        return res.sendFile(path.join(__dirname, 'views', 'profile.html'))
+      })
     })
     .then(() => {
-        // API routes
-        app.get("/greetings", (req, res) => {
-            return res.status(200).json({
-                message: "Hello World!"
-            })
+      // API routes
+      app.get('/greetings', (req, res) => {
+        return res.status(200).json({
+          message: 'Hello World!'
         })
+      })
 
-        app.use("/api/auth", authRouter)
+      app.use('/api/auth', authRouter)
     })
     .then(() => {
-        server = app.listen(8000, (error) => {
-            if (error) throw error
-            console.info("server is running on port 8000")
-        })
+      server = app.listen(8000, (error) => {
+        if (error) throw error
+        console.info('server is running on port 8000')
+      })
     })
     .then(() => {
-        // declare globals here
-        global.app = app
+      // declare globals here
+      global.app = app
 
-        gracefulShutdown(server)
+      gracefulShutdown(server)
     })
     .catch(error => {
-        console.error(error)
-        shutDownHandler(server)
+      console.error(error)
+      shutDownHandler(server)
     })
 }
 
--- utils.js
+++ utils.js
@@ -1,32 +1,32 @@
 
 const shutDownHandler = (server) => {
-    if (server) {
-        console.info("initiating graceful server shutdown")
-        server.close(() => {
-            console.log('server closed')
-            if (global.app && global.app.db) {
-                global.app.db.connection.close(false, () => console.info("database connection closed"))
-            }
-            process.exit(0)
-        })
-        return
-    }
-    console.warn("initiating hard server shutdown")
-    process.exit(1)
+  if (server) {
+    console.info('initiating graceful server shutdown')
+    server.close(() => {
+      console.log('server closed')
+      if (global.app && global.app.db) {
+        global.app.db.connection.close(false, () => console.info('database connection closed'))
+      }
+      process.exit(0)
+    })
+    return
+  }
+  console.warn('initiating hard server shutdown')
+  process.exit(1)
 }
 
 const gracefulShutdown = (server) => {
-    process.on("SIGINT", () => {
-        console.error("SIGINT signal recieved")
-        shutDownHandler(server)
-    })
-    process.on("SIGTERM", () => {
-        console.error("SIGTERM signal recieved")
-        shutDownHandler(server)
-    })
+  process.on('SIGINT', () => {
+    console.error('SIGINT signal recieved')
+    shutDownHandler(server)
+  })
+  process.on('SIGTERM', () => {
+    console.error('SIGTERM signal recieved')
+    shutDownHandler(server)
+  })
 }
 
 module.exports = {
-    shutDownHandler,
-    gracefulShutdown
+  shutDownHandler,
+  gracefulShutdown
 }
